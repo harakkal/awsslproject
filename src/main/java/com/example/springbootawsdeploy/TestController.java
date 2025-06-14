f5eb36cf7b46533e43a524ec22aa9d40e96073da
Hanas Arakkal <134443127+harakkal@users.noreply.github.com>
2025-06-14 20:14:41 +0400

    Java file updated to test CI/CD pipeline

--- src/main/java/com/example/springbootawsdeploy/TestController.java
+++ src/main/java/com/example/springbootawsdeploy/TestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/data")
     public String getData() {
-        return "First message from AWS Ecs (hanas)";
+        return "Modified message from AWS ECS deployed by Hanas" ;
     }
 
     @GetMapping("/message")
